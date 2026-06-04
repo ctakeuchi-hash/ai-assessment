00b538233e24ee726c8e68fb6e2d5723f88a8a91
ctakeuchi-hash <ctakeuchi@gmail.com>
2026-06-04 13:49:56 -0700

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = { title: 'AI Business Assessment' }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
